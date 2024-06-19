0e717d5f49db298e361cddc7d4868b09a8c4f79d
Raymond <20248577+javaarchive@users.noreply.github.com>
2024-06-18 20:15:31 -0700

    fix colors part 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,8 +21,9 @@ module.exports = {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        "background-lighter": "var(--background-lighter)",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
