f463403de4840b60622837b46eca35c58a7bb769
Raymond <20248577+javaarchive@users.noreply.github.com>
2024-06-18 20:27:25 -0700

    fix default

--- src/views/Player.jsx
+++ src/views/Player.jsx
@@ -170,8 +170,8 @@ const Player = () => {
                     >
                       <div className="col-span-10 bg-background-lighter"></div>
                       <div className="col-span-2 bg-background-lighter">
-                        <Tabs defaultValue="account" className="w-[400px]">
-                          <TabsList>
+                        <Tabs defaultValue="session" className="w-full">
+                          <TabsList className = "w-full">
                             <TabsTrigger value="session">Session</TabsTrigger>
                             <TabsTrigger value="social">Social</TabsTrigger>
                           </TabsList>
