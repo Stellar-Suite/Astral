ab231715643800d54e6d96120efce29df18ca41b
Raymond <20248577+javaarchive@users.noreply.github.com>
2024-07-05 14:12:09 -0700

    Update streamer_client.jsx

--- src/client/streamer_client.jsx
+++ src/client/streamer_client.jsx
@@ -1,11 +1,35 @@
+import { io } from "socket.io-client";
+import { getApiUrl } from "utils/api";
+import { getJwt } from "utils/login";
+
 class StreamerPeerConnection {
     constructor(sid, options){
         this.sid = sid;
         this.options = options;
     }
 
+    socketConnectedOnce = false;
+    socket;
+
+    onSocketFirstConnect() {
+        if(getJwt()){
+            this.socket.emit("jwt", getJwt());
+        }else{
+            console.warn("socket did not auth because we have no jwt creds");
+        }
+    }
+
+    onSocketConnect() {
+    }
+
     start() {
-        
+        this.socket = io(getApiUrl());
+        this.socket.on("connect", () => {
+            if(!this.socketConnectedOnce){
+                this.onSocketFirstConnect();
+            }
+            this.onSocketConnect();
+        });
     }
 
     parent = null;
