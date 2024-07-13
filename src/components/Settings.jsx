25d7c066bfab663fc3fe4c64915bc819140536fc
Raymond <20248577+javaarchive@users.noreply.github.com>
2024-07-13 15:52:12 -0700

    make dialog closable

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,25 +1,37 @@
 import { Button } from "../@/components/ui/button";
-import { Dialog, DialogContent, DialogTrigger, DialogTitle, DialogDescription, DialogHeader, DialogFooter } from "../@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogTitle,
+  DialogDescription,
+  DialogHeader,
+  DialogFooter,
+  DialogClose
+} from "../@/components/ui/dialog";
 import React from "react";
 
 export function Settings(props) {
   return (
     <Dialog>
-      <DialogTrigger className={props.className}>{props.children || "Settings"}</DialogTrigger>
+      <DialogTrigger className={props.className}>
+        {props.children || "Settings"}
+      </DialogTrigger>
       <DialogContent>
         <DialogHeader className={""}>
           <DialogTitle>Edit Settings</DialogTitle>
           <DialogDescription>
-            These settings are specific to this session. Syncing is not yet implemented.
+            These settings are specific to this session. Syncing is not yet
+            implemented.
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-
-          </div>
+          <div className="grid grid-cols-4 items-center gap-4"></div>
         </div>
         <DialogFooter className={""}>
-          <Button type="submit">Save</Button>
+          <DialogClose asChild>
+            <Button type="submit">Save</Button>
+          </DialogClose>
         </DialogFooter>
       </DialogContent>
     </Dialog>
